68662b1ec34b0ec4043ec6357dd6d9bb84c4acd0
Wilber Ulloa <wuj890312@gmail.com>
2021-01-04 16:28:47 -0500

    exercise 2.2 and 2.3

--- part2/src/components/Content.js
+++ part2/src/components/Content.js
@@ -2,11 +2,19 @@ import React from 'react';
 import Part from "./Part";
 
 const Content = ({ parts }) => {
+
+    const total = () => {
+       return parts.map(part => part.exercises).reduce((acc, curr) => { 
+            return acc + curr
+        })
+    }
+
     return (
       <div>
         {parts.map(part => (
             <Part key={part.id} part = {part}/>
         ))}
+        <p>Total of {total()} exercises</p>
       </div>
     )
   }
