7acf758a62215748797c2a7b0d85811acf84eaad
Wilber Ulloa <wilber@fonoma.com>
2021-02-06 17:13:41 -0500

    exec 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,21 @@
-import React, { useState} from 'react'
+import React, { useState, useEffect} from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
-
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const url = "http://localhost:3002/persons"
+
+  useEffect(() => {
+    axios.get(url)
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
 
   return (
     <div>
